34fc0d2b2f582c3ea10bf269f43dc08f0f7405b6
Marcos Adriano <marcosadriano740@gmail.com>
2021-07-22 20:02:01 -0300

    Setup database done

--- src/infra/database/createPostsTable.js
+++ src/infra/database/createPostsTable.js
@@ -3,7 +3,7 @@ const database = require('./connect')
 module.exports = async () => {
   const db = await database()
 
-  await db.exec(`CREATE TABLE posts (
+  await db.exec(`CREATE TABLE IF NOT EXISTS posts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
     comments TEXT,
--- src/server.js
+++ src/server.js
@@ -8,10 +8,8 @@ app.use(cors())
 app.use(express.json())
 app.use(route)
 
-try {
+{
   (async () => await createPostsTable())()
-} catch (error) {
-  console.log(error)
 }
-
+  
 app.listen(3333, () => console.log('Server on'))
